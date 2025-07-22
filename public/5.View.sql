6958975d533a876e32b17e5bfb2e8c788d9f130a
JackQIao <qiaoweipeng@outlook.com>
2025-07-23 05:16:27 +0800

    View已学完

--- public/5.View.sql
+++ public/5.View.sql
@@ -27,19 +27,15 @@ Views 视图
         5. 可更新视图
             如果视图中没使用过GROUP BY、聚合函数、HAVING、DISTINCT、UNION，则为可更新视图。
             可更新视图不但可以用在SELECT中，还可以用在INSERT、DELETE、UPDATE语句中。
+            另外，INSERT还要满足附加条件：视图必须包含底层原表的所有必须字段。
         6. WITH CHECK OPTION
             在创建视图末尾加上WITH CHECK OPTION ，视图不会因为修改某些字段的值而导致某行数据消失。
             如：在创建视图时有个限制条件name > 30,你使用UPDATE将name修改为20，当前数据肯定会从视图消失。但是如果
             在创建视图末尾加上WITH CHECK OPTION，你执行该语句，会报错，表示不允许修改。
-
-了解这些优点，但不要盲目将他们运用在所有的情形中。
 */
 
-# 1. 创建View
-
-# demo1：创建sales_by_client视图
-USE sql_invoicing;
 # 创建View
+USE sql_invoicing;
 CREATE VIEW sales_by_client AS
 SELECT
     client_id,
@@ -50,11 +46,12 @@ FROM
         JOIN invoices i USING (client_id)
 GROUP BY
     client_id, name;
-# 若要删掉该视图用 DROP VIEW sales_by_client 或 菜单栏找到对应的视图右键删除
-DROP VIEW sales_by_client;
-# 创建视图后可就当作sql_invoicing库下一张表一样进行各种操作
+
 # 使用View
-USE sql_invoicing;
+SELECT *
+FROM
+    sales_by_client;
+
 SELECT
     s.name,
     s.total_sales,
@@ -65,40 +62,33 @@ FROM
 WHERE
     s.total_sales > 500;
 
+# 删除view
+DROP VIEW sales_by_client;
 
-# demo2：创建一个客户差额表视图，可以看到客户的id，名字以及差额（发票总额-支付总额）
-USE sql_invoicing;
 # 创建视图
+# 创建一个客户差额表视图，可以看到客户的id，名字以及差额（发票总额-支付总额）
+USE sql_invoicing;
 CREATE VIEW client_blance AS
 SELECT
     client_id,
     c.name,
-    SUM(invoice_total - payment_total) AS balanse
+    SUM(invoice_total - payment_total) AS balance
 FROM
     clients c
         JOIN invoices i USING (client_id)
 GROUP BY
     client_id;
 
-# 使用视图
-SELECT *
-FROM
-    client_blance;
-
-# 2. 更新或删除View
-
-/*
-    1. 修改视图可以先 DROP 再 CREATE，但最好是用 CREATE OR REPLACE
-    2. 视图的SQL可以在菜单栏对应视图右键，下查看和修改，最好还是保存为sql文件Git管理。
-*/
-# demo：想在上一节的顾客差额视图的查询语句最后加上按差额降序排列
-
-# 方法1：先drop再create
+# 修改视图
+# 将client_blance视图修改为 按差额 降序排列
 USE sql_invoicing;
-DROP VIEW client_blance;
-# 若不存在这个视图，用DROP会报错，最好加上 IF EXISTS，后面会讲
 
-CREATE VIEW client_blance AS
+# 方法1. 先 DROP 再 CREATE
+DROP VIEW client_blance;# 若不存在这个视图，用DROP会报错，最好加上 IF EXISTS，后面会讲
+# CREATE VIEW client_blance AS
+
+# 方法2. CREATE OR REPLACE
+CREATE OR REPLACE VIEW client_blance AS
 SELECT
     client_id,
     c.name,
@@ -110,4 +100,55 @@ GROUP BY
     client_id
 ORDER BY
     balanse DESC;
-# 方法2：菜单栏中找到对应的View修改，直接修改SQL即可。
+
+# 可更新视图的演示：
+
+# 创建视图
+USE sql_invoicing;
+CREATE OR REPLACE VIEW invoices_with_balance AS
+SELECT
+    invoice_id,
+    number,
+    client_id,
+    invoice_total,
+    payment_total,
+    invoice_date,
+    invoice_total - payment_total AS balance,
+    due_date,
+    payment_date
+FROM
+    invoices
+WHERE
+    (invoice_total - payment_total) > 0
+WITH CHECK OPTION;
+# invoices_with_balence视图满足条件，是可更新视图，可以增删改：
+
+# 1. DELETE
+# 删掉id为1的发票记录
+DELETE
+FROM
+    invoices_with_balance
+WHERE
+    invoice_id = 1;
+# 2. UPDATE
+# 将2号发票记录的期限延后两天
+UPDATE invoices_with_balance
+SET
+    due_date = DATE_ADD(due_date, INTERVAL 2 DAY)
+WHERE
+    invoice_id = 2;
+# 3. INSERT
+# 在视图中用INSERT新增记录的话还有另一个前提，即视图必须包含其底层所有原始表的所有必须字段（这很好理解）
+# 例如，若这个invoices_with_balance视图里没有invoice_date字段（invoices中的必须字段），那就无法通过该
+# 视图向invoices表新增记录，因为invoices表不会接受必须字段invoice_date为空的记录
+
+# 通过视图新增数据比较复杂切不可靠，一般不会这么操作
+
+#  WITH CHECK OPTION
+# 创建invoices_with_balance视图时，末尾没有添加WITH CHECK OPTION,以下SQL语句可以执行成功，并且对应的数据会从视图消失。因为符合视图中的筛选条件。
+#                  如果末尾添加WITH CHECK OPTION，以下SQL语句执行会报错，不允许修改。
+UPDATE invoices_with_balance
+SET
+    invoice_total = payment_total
+WHERE
+    invoice_id = 2;
